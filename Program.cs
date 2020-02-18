fba27dfaade137b9d99950094695025ef45064b4
Kaynon Martin <kaynonmartin@Kaynons-MacBook-Pro.local>
2020-02-18 18:44:58 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpClassProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
