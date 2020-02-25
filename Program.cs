1eb6108ebdc6fea3b640a1edd1d7e6cde731999c
Kaynon Martin <kaynonmartin@Kaynons-MacBook-Pro.local>
2020-02-25 18:59:38 -0500

    Secondary commt

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using src;
 
 namespace CSharpClassProject
 {
@@ -6,7 +7,73 @@ namespace CSharpClassProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+         bool keepLooping = true; 
+
+          while(keepLooping) {
+        
+
+Console.WriteLine("Enter Student Id");
+ var studentId = Convert.ToInt32(Console.ReadLine());
+
+
+
+Console.WriteLine("Enter First Name");
+var studentFirstName = Console.ReadLine();
+
+
+
+
+Console.WriteLine("Enter Last Name");
+var studentLastName = Console.ReadLine();
+
+
+
+
+Console.WriteLine("Enter Class Name");
+var className = Console.ReadLine();
+
+
+
+
+Console.WriteLine("Enter Last Class Completed");
+var lastClass = Console.ReadLine();
+
+
+
+
+Console.WriteLine("Enter Last Class Completed Date in format MM/dd/YYYY");
+var lastCompletedOn = DateTimeOffset.Parse(Console.ReadLine());
+
+
+
+
+Console.WriteLine("Enter Start Date in format MM/dd/YYYY");
+var startDate = DateTimeOffset.Parse(Console.ReadLine());
+
+
+
+
+var studentRecord = new Student();
+studentRecord.StudentId = studentId;
+studentRecord.FirstName = studentFirstName;
+studentRecord.LastName = studentLastName;
+studentRecord.ClassName = className;
+studentRecord.StartDate = startDate;
+studentRecord.LastClassCompleted = lastClass;
+studentRecord.LastClassCompletedOn = lastCompletedOn;
+
+Console.WriteLine($"Student Id | Name |  Class "); ;
+
+Console.WriteLine($"{studentRecord.StudentId} | {studentRecord.FirstName} {studentRecord.LastName} | {studentRecord.ClassName} "); ;
+
+Console.ReadKey(); 
+
+          }
+
+
+}
+
         }
     }
-}
+
